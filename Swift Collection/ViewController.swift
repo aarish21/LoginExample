f2f3c59195ce17656d85d3e9454c3ad8cc9acee7
aarish21 <aarish.jerry@gmail.com>
2022-08-10 11:34:52 +0530

    Initial Commit

---
+++ Swift Collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Collection
+//
+//  Created by Aarish on 10/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
